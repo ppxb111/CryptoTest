80aa8a88c66e32d94eda411766ce658efca46486
ppxb111 <88035779+ppxb111@users.noreply.github.com>
2026-04-28 14:50:05 +0800

    Update crypto.swift

--- crypto.swift
+++ crypto.swift
@@ -1,18 +1,22 @@
 import Foundation
 import CryptoKit
+import os.log 
 
 public func runCrypto() {
-    print("CryptoKit 测试...")
-        print("准备生成")
-        var checksum: UInt64 = 0
-        for i in 1...1_000_000 {
-            let key = SymmetricKey(size: .bits256)
-            let data = key.withUnsafeBytes { $0.load(as: UInt64.self) }
-            checksum ^= data
-            if i % 200_000 == 0 {
-                print("已生成 \(i) 个密钥...")
-            }
+    let log = OSLog(subsystem: "com.yourcompany.crypto", category: "Performance")
+    
+    os_log("CryptoKit 测试...", log: log, type: .info)
+    os_log("准备生成", log: log, type: .info)
+    
+    var checksum: UInt64 = 0
+    for i in 1...1_000_000 {
+        let key = SymmetricKey(size: .bits256)
+        let data = key.withUnsafeBytes { $0.load(as: UInt64.self) }
+        checksum ^= data
+        if i % 200_000 == 0 {
+            os_log("已生成 %d 个密钥...", log: log, type: .info, i) 
         }
-        print("测试完成！")
+    }
+    
+    os_log("测试完成！", log: log, type: .info)
 }
-
